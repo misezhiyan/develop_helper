16c742933eec08a97a7f13066eb960107bba8a87
kimmy <misezhiyan@163.com>
2023-06-13 17:47:50 +0800

    一版

--- web/src/main/java/com/kimmy/learn/web/WebApplication.java
+++ web/src/main/java/com/kimmy/learn/web/WebApplication.java
@@ -2,12 +2,14 @@ package com.kimmy.learn.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class WebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WebApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(WebApplication.class, args);
+        System.out.println("http://localhost:" + context.getEnvironment().getProperty("server.port") + "/");
     }
 
 }
