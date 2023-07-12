0e1122f1be0314b470dc8211b0fc01e0e95078e2
kimmy <misezhiyan@163.com>
2023-07-12 09:14:20 +0800

    'daydayup'

--- daydayup/src/main/java/com/daydayup/vo/request/StudentListRequest.java
+++
@@ -1,9 +0,0 @@
-package com.daydayup.vo.request;
-
-import com.daydayup.entity.Student;
-import org.springframework.stereotype.Component;
-
-@Component
-public class StudentListRequest extends Student {
-
-}
